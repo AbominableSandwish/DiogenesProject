eb71df43aac5a7186c14575f3107f3299e18b108
Dylan <dylan_vonarx@hotmail.fr>
2025-08-31 10:13:54 +0200

    feat: Add GameObject reference and constructor to Engine

--- Assets/Scripts/Map/Stucture/Utility/Device/Engine.cs
+++ Assets/Scripts/Map/Stucture/Utility/Device/Engine.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 [Serializable]
 public class Engine : Structure
 {
+    protected GameObject _object;
+
     public float _electricityConsumption = 0.0f;
     public float _electricityCurrent = 0.0f;
     public float EnginePerformance = 0.0f;
+
+    public Engine(Tilemap tilemap = null, int pos_x = 0 , int pos_y = 0)
+    {
+        if(tilemap != null)
+        {
+            _object = tilemap.GetInstantiatedObject(new Vector3Int(pos_x, pos_y));
+        }
+    }
+
     public virtual void Consumption(float _electricity)
     {
         _electricityCurrent = _electricity;
